21010e8a080f1b5ca9accb5863264584ba0468f4
MichaelHayball <Hayball2k@hotmail.com>
2024-02-07 19:00:18 +0000

    Added CreateMatch unit tests to check fringe cases.

--- scoreboard/Scoreboard.Tests/ScoreboardTests.cs
+++ scoreboard/Scoreboard.Tests/ScoreboardTests.cs
@@ -1,5 +1,9 @@
 using AutoFixture;
 using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+using FluentAssertions.Execution;
+using FluentAssertions;
+using Xunit;
 
 namespace Scoreboard.Tests
 {
@@ -23,7 +27,72 @@ namespace Scoreboard.Tests
         {
             public CreateMatchTests()
             {
+            }
 
+            [Theory, AutoData]
+            public void CreateMAtch_Creates_Match_Ok(string homeTeamName, string awayTeamName)
+            {
+                // Arrange
+                // Act
+                var action = () => _objectToTest.CreateMatch(homeTeamName, awayTeamName);
+                // Assert
+                using (new AssertionScope())
+                {
+                    action.Should().NotThrow();
+                    var result = action.Invoke();
+                    result.Should().BeTrue();
+                }
+            }
+
+            [Theory]
+            [InlineAutoData("", "away")]
+            [InlineAutoData(null, "away")]
+            public void CreateMatch_Returns_ArgumentNullException_When_homeTeamName_is_NullOrEmpty(string homeTeamName, string awayTeamName)
+            {
+                // Arrange
+                // Act
+                var action = () =>_objectToTest.CreateMatch(homeTeamName, awayTeamName);
+                // Assert
+                using ( new AssertionScope())
+                {
+                    action.Should().Throw<ArgumentException>()
+                        .WithMessage("homeTeamName cannot be null or empty");
+                }
+            }
+
+            [Theory]
+            [InlineAutoData("home", "")]
+            [InlineAutoData("home", null)]
+            public void CreateMatch_Returns_ArgumentNullException_When_awayTeamName_is_NullOrEmpty(string homeTeamName, string awayTeamName)
+            {
+                // Arrange
+                // Act
+                var action = () => _objectToTest.CreateMatch(homeTeamName, awayTeamName);
+                // Assert
+                using (new AssertionScope())
+                {
+                    action.Should().Throw<ArgumentException>()
+                        .WithMessage("awayTeamName cannot be null or empty");
+                }
+            }
+
+            [Theory]
+            [InlineAutoData("Team A", "Team A")]
+            [InlineAutoData("Team A", "Team A ")]
+            [InlineAutoData("Team A", " Team A ")]
+            [InlineAutoData("Team A", "Team a")]
+            [InlineAutoData("Team A", "team A")]
+            public void CreateMatch_Returns_ArgumentException_When_TemaNames_Match_Regardless_of_WhiteSpace(string homeTeamName, string awayTeamName)
+            {
+                // Arrange
+                // Act
+                var action = () => _objectToTest.CreateMatch(homeTeamName, awayTeamName);
+                // Assert
+                using (new AssertionScope())
+                {
+                    action.Should().Throw<ArgumentException>()
+                        .WithMessage("homeTeamName cannot be the same as awayTeamName");
+                }
             }
         }
 
